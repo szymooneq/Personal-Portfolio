459affe911c74d76b123df4c8f867f704266ba40
Szymon Dudka <szymooneq@zohomail.eu>
2022-11-23 13:18:59 +0100

    favicon fix, scale fix, Inter font fix

--- components/Layout/RootLayout.jsx
+++ components/Layout/RootLayout.jsx
@@ -9,11 +9,14 @@ export default function RootLayout({ children }) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#1D1D20" />
-        <link rel="apple-touch-icon" href="/logo192.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1d1d20" />
+        <meta name="msapplication-TileColor" content="#1d1d20" />
+        <meta name="theme-color" content="#1d1d20" />
       </Head>
       
       <NextNProgress color="#5d5dff" height={3} />
